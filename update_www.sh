46fc23830dca1b6286807fb7adb794ebccb980a6
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2016-05-18 16:37:51 +0300

    code cleanup

--- update_www.sh
+++ update_www.sh
@@ -2,15 +2,15 @@
 
 # Add the current user to the www-data group if not there
 if groups "$USER" | grep -q -E ' www-data(\s|$)'; then
-    echo $USER ' is in www-data...moving on'
+    echo $USER 'is in www-data...moving on'
 else
 	sudo adduser $USER www-data
-	    echo 'Added ' $USER ' to www-data...'
+	    echo 'Added '$USER' to www-data...'
 fi
 
 # Current user to own /var/www
 sudo chown $USER:www-data -R /var/www
-	echo $USER ' now owns /var/www...'
+	echo $USER 'now owns /var/www...'
 
 # Change permission of /var/www to 755
 sudo chmod u=rwX,g=srX,o=rX -R /var/www
