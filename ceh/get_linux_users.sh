92ca29458db0d398046ac59bc79ff6a8d4808bf8
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2016-07-12 14:43:54 +0300

    added script to get linux users

---
+++ ceh/get_linux_users.sh
@@ -0,0 +1,9 @@
+## get min UID limit ##
+l=$(grep "^UID_MIN" /etc/login.defs)
+ 
+## get max UID limit ##
+l1=$(grep "^UID_MAX" /etc/login.defs)
+ 
+## use awk to print if UID >= $MIN and UID <= $MAX   ##
+awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max ) print $0}' /etc/passwd
+
