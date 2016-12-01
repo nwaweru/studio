d8d469f4a694b8309e690ce49fb9f6e11ac77ec9
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2016-12-01 11:28:07 +0300

    updated www setup

--- setup_www.sh
+++ setup_www.sh
@@ -3,21 +3,17 @@
 # Alert execution start
 echo 'Setup initiated...'
 
-# Set root to own /var/www folder
-sudo chown root:root /var/www
-# Add current user to www-data group
-sudo usermod -a -G www-data $USER
-# Set web server as owner of all folders within /var/www
-sudo chown -R www-data:www-data /var/www/*
-# Set current user as owner of all public_html folders
-sudo chown -R $USER:$USER /var/www/*/public_html
+# Set www-data to own /var/www folder
+sudo chown -R www-data:www-data /var/www
+# Add current user to the www-data group
+sudo usermod -aG www-data $USER
 # Set permissions for files in /var/www
 sudo find /var/www/* -type f -exec chmod 664 {} \;
 # Set permissions for folders in /var/www
 sudo find /var/www/* -type d -exec chmod 775 {} \;
 
 # Start Laravel setup
-sudo chmod -R ug+rwx /var/www/*/storage /var/www/*/bootstrap/cache
+sudo chmod -R 770 /var/www/*/storage /var/www/*/bootstrap/cache
 # End Laravel setup
 
 # Confirm completion
