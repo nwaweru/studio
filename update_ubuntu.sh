9527eebd50c214526c962f0a795c097f961ad218
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2018-01-18 19:52:29 +0300

    updated query to check if youtube-dl is installed

--- update_ubuntu.sh
+++ update_ubuntu.sh
@@ -6,5 +6,7 @@ sudo apt-get autoremove && sudo apt-get autoclean
 # Update
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
 
-# Update youtube-dl
-sudo youtube-dl -U
+# Check if youtube-dl is installed and update it
+if dpkg-query -l youtube-dl; then
+	sudo youtube-dl -U
+fi
