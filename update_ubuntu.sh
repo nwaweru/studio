6e4975af63fcdc188677e6bc1a34c95c847162e3
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2018-03-27 18:38:33 +0300

    updated youtube-dl update

--- update_ubuntu.sh
+++ update_ubuntu.sh
@@ -6,7 +6,5 @@ sudo apt-get autoremove && sudo apt-get autoclean
 # Update
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
 
-# Check if youtube-dl is installed and update it
-if dpkg-query -l youtube-dl; then
-	sudo youtube-dl -U
-fi
+# Update youtube-dl | Remove this if you do not use youtube-dl
+sudo youtube-dl -U
