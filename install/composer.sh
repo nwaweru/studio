fe8014481d5132d0a2153a9098f16c181a6d31e3
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2018-09-03 15:18:13 +0300

    updated composer installation script

--- install/composer.sh
+++ install/composer.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('SHA384', 'composer-setup.php');")"
 
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
 then
@@ -14,5 +14,4 @@ fi
 php composer-setup.php --quiet
 RESULT=$?
 rm composer-setup.php
-sudo mv composer.phar /usr/local/bin/composer
 exit $RESULT
