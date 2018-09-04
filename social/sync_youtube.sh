f448930417470d724f9bbe4b82d4f1328267e9bc
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2018-09-04 09:44:13 +0300

    added youtube-dl playlist sync

---
+++ social/sync_youtube.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# alias youtube="sh ~/scripts/social/sync_youtube.sh"
+
+echo "starting sync."
+
+# your variables
+playlist="https://www.youtube.com/playlist?list=PL67Km-4Kw3GQUo1MwlSvNh7IIwry0oafY"
+options="-i --extract-audio --audio-format mp3"
+downloadFolder="/home/$USER/Music/YouTube"
+
+# remove existing files
+rm -rf $downloadFolder && mkdir $downloadFolder
+
+# download the playlist with the defined options
+cd $downloadFolder && youtube-dl $options $playlist
+
+echo "coolio. your local playlist has been synced."
