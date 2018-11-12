8e25267290c775768f5f801cf70f34368f8a8046
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2018-11-12 14:50:39 +0300

    updated for ubuntu 18.04

--- dev/update.ubuntu.sh
+++ dev/update.ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Cleanup before update
-sudo apt-get autoremove && sudo apt-get autoclean
+sudo apt autoremove && sudo apt autoclean
 
 # Update
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
 
 # Update youtube-dl | Remove this if you do not use youtube-dl
 sudo youtube-dl -U
