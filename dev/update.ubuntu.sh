1c6d86cbf2bd13a314f3dce9e40f7051b27b64b6
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2019-10-23 18:55:42 +0300

    update snaps too

--- dev/update.ubuntu.sh
+++ dev/update.ubuntu.sh
@@ -6,5 +6,8 @@ sudo apt autoremove && sudo apt autoclean
 # Update
 sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
 
+# Update snaps
+sudo snap refresh
+
 # Update youtube-dl | Remove this if you do not use youtube-dl
 sudo youtube-dl -U
