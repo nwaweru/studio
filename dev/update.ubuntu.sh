76dc07ccb6bc109999d96db1cbba49dc911f53ca
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2019-12-09 16:24:34 +0300

    cleared comments

--- dev/update.ubuntu.sh
+++ dev/update.ubuntu.sh
@@ -6,8 +6,5 @@ sudo apt autoremove && sudo apt autoclean
 # Update
 sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
 
-# Update snaps
-# sudo snap refresh
-
 # Update youtube-dl | Remove this if you do not use youtube-dl
 sudo youtube-dl -U
