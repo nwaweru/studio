02595a95e6d62b0df597b970dcabaf37d930a151
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2020-02-03 13:41:03 +0300

    added brave install

---
+++ dev/install/brave.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###################################
+# Install Brave Browser
+###################################
+
+sudo apt install apt-transport-https curl
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install -y brave-browser
