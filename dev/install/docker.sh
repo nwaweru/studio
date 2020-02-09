8e217cd9c244b599cac2039f7f9f5cde41fe1f66
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2020-02-09 14:59:17 +0300

    added docker installer

---
+++ dev/install/docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+###################################
+# Install Docker [CE]
+###################################
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker ${USER}
+su - ${USER}
+sudo systemctl status docker
