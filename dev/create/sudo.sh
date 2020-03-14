4adabe6ee81f56dbca93ae343c0ba34a19a44777
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2020-03-14 23:46:56 +0300

    patches

--- dev/create/sudo.sh
+++ dev/create/sudo.sh
@@ -9,7 +9,7 @@ usermod -aG sudo $1
 # copy .ssh directory to allow access using current setup.
 # TODO: optional to enable creation of OTHER users.
 echo 'Enabling ssh for '$1'...'
-rm -rf /home/$1/.ssh
+# rm -rf /home/$1/.ssh
 cp -r /root/.ssh /home/$1
 chown -R $1:$1 /home/$1/.ssh
 echo $1' created successfully.'
