7a16e835a48393de61125dd140499b466502ae2f
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2020-03-15 00:28:53 +0300

    patches

--- dev/create/ssh-key.sh
+++ dev/create/ssh-key.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# rsa
 ssh-keygen -t rsa -b 4096 -C "ndiranguwaweru@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
+
+# ed25519
+ssh-keygen -t ed25519 -C "ndiranguwaweru@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519.pub
