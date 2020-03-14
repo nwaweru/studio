bcdb251d140cb5c00852c1d6173cbd7341defc76
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2020-03-14 23:39:04 +0300

    patches

--- dev/create/sudo.sh
+++ dev/create/sudo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # create a new sudo user
-echo 'Creating user $1...'
+echo 'Creating user '$1'...'
 adduser $1
-echo 'Adding $1 to the sudoers group...'
+echo 'Adding '$1' to the sudoers group...'
 usermod -aG sudo $1
 
 # copy .ssh directory to allow access using current setup.
 # TODO: optional to enable creation of OTHER users.
-echo 'Enabling ssh for $1...'
+echo 'Enabling ssh for '$1'...'
 rm -rf /home/$1/.ssh
 cp -r /root/.ssh /home/$1
 chown -R $1:$1 /home/$1/.ssh
-echo '$1 created successfully.'
+echo $1' created successfully.'
