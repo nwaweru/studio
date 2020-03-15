1ef60ca19b89bafc7f8037e843856d3ec84450ec
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2020-03-15 03:08:13 +0300

    patches

--- dev/install/docker/debian-10.sh
+++ dev/install/docker/debian-10.sh
@@ -14,7 +14,7 @@ sudo apt install docker-ce
 
 cd /tmp
 curl -O https://download.docker.com/linux/ubuntu/dists/bionic/pool/edge/amd64/containerd.io_1.2.2-3_amd64.deb
-$ sudo apt install ./containerd.io_1.2.2-3_amd64.deb
+sudo apt install ./containerd.io_1.2.2-3_amd64.deb
 cd
 
 sudo systemctl status docker
