df25400d8dea6d92be649c102ad9c39fb4270551
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2020-03-15 03:02:12 +0300

    refactored

---
+++ dev/install/docker/debian-10.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+###################################
+# Install Docker [CE]
+###################################
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
+sudo systemctl status docker
