659fdda646d0a460bc380d0a38b8e8466d6ec6b1
Ndirangu Waweru <ndiranguwaweru@gmail.com>
2020-03-16 19:57:54 +0300

    added php7.4 installation for debian 10

---
+++ dev/debian/install/php7-4.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# setup
+sudo apt -y install lsb-release apt-transport-https ca-certificates 
+sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo apt update
+
+# installation
+sudo apt -y install php7.4
+
+# additional packages
+sudo apt-get install php7.4-{bcmath,bz2,intl,gd,mbstring,mysql,zip}
