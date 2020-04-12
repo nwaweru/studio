bbdc4aa821e709f309b70979e268bf3771b9e4b6
Ndirangu Waweru <>
2020-04-12 19:23:06 +0300

    wip

--- ubuntu/install/youtube-dl.sh
+++ ubuntu/install/youtube-dl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo apt install -y curl python-minimal
+sudo apt install -y curl python-minimal ffmpeg
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
